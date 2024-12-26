4170bdc76eb8712bc10d263d485a899049107a7c
Antonio Labinjan <alabinjan6@gmail.com>
2024-12-26 15:47:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import tasksRouter from "./routes/tasks.js"
+
+const PORT = 8000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", tasksRouter);
+
+app.get("/", (req, res) => {
+  res.send("TaskManagerBackend");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Poslužitelj sluša na portu ${PORT}`);
+});
